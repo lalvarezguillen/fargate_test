834d3e6a76231fae77c5f9fa2972258c09a2b346
Luis <lalvarezguillen@gmail.com>
2018-02-10 08:18:52 +0100

    the server takes the listening port from environment variable fargate_test_port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const process = require('process');
 const server = require('server');
 const { get, post } = server.router;
 
 
-server({ port: 8080 }, 
+
+server({ port: process.env.fargate_test_port || 80 }, 
     get('/', ctx => 'Hello, from Fargate!'))
